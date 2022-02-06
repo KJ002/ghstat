7fa514b3f6e8956731da65183b968ae42ccd29c1
James Butcher <jamesbutcher167@gmail.com>
2022-02-06 16:44:27 +0000

    Clean up get_github_data

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 extern crate chrono;
 
-use serde_json::json;
 use chrono::prelude::*;
+use serde_json::json;
 
 use std::fs::File;
-use std::io::{Write, Read};
+use std::io::{Read, Write};
 
 fn read_cache() -> Option<serde_json::Value> {
     let mut buffer = String::new();
 
-    let mut file = match File::open("/home/james/.ghstats/cache.json") {
+    let mut file = match File::open("/home/james/.ghstat/cache.json") {
         Ok(handle) => handle,
-        Err(_) => return None
+        Err(_) => return None,
     };
 
     let json_str = match file.read_to_string(&mut buffer) {
         Ok(_) => buffer.as_str(),
-        Err(_) => return None
+        Err(_) => return None,
     };
 
     match serde_json::from_str(json_str) {
         Ok(result) => Some(result),
-        Err(_) => None
+        Err(_) => None,
     }
 }
 
 fn get_github_data() -> serde_json::Value {
-     let mut result: serde_json::Value = match ureq::get("https://api.github.com/users/KJ002").call() {
+    let mut result = match ureq::get("https://api.github.com/users/KJ002").call() {
         Ok(response) => response.into_json::<serde_json::Value>().unwrap(),
-        Err(_) => panic!("Error")
+        Err(_) => panic!("Error"),
     };
 
     result["test"] = json!(Local::now().timestamp());

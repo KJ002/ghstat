bec64573e442df227a2adee0ed5d4353c7f5fca0
James Butcher <jamesbutcher167@gmail.com>
2022-02-07 21:21:05 +0000

    Convert json_stdout to a trait and implementation

--- src/main.rs
+++ src/main.rs
@@ -56,18 +56,24 @@ fn safe_read() -> serde_json::Value {
     }
 }
 
-fn json_stdout(key: &str, json: &serde_json::Value) {
-    fn operations(value: &Value) {
-        match value {
-            Value::Null => println!("Null"),
-            Value::Bool(x) => println!("{}", x),
-            Value::Number(x) => println!("{}", x),
-            Value::String(x) => println!("{}", x),
-            Value::Array(x) => x.iter().map(operations).collect::<()>(),
-            Value::Object(x) => x.values().map(operations).collect::<()>()
-        };
+trait DisplayJson {
+    fn json_stdout(&self, key: &str);
+}
+
+impl DisplayJson for serde_json::Value {
+    fn json_stdout(&self, key: &str) {
+        fn operations(value: &Value) {
+            match value {
+                Value::Null => println!("Null"),
+                Value::Bool(x) => println!("{}", x),
+                Value::Number(x) => println!("{}", x),
+                Value::String(x) => println!("{}", x),
+                Value::Array(x) => x.iter().map(operations).collect::<()>(),
+                Value::Object(x) => x.values().map(operations).collect::<()>()
+            };
+        }
+        operations(&self[key])
     }
-    operations(&json[key])
 }
 
 fn main() {
@@ -81,5 +87,5 @@ fn main() {
         content = safe_read()
     }
 
-    json_stdout("test", &content)
+    content.json_stdout("ghstats_timestamp")
 }
